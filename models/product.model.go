f3b4f3b426e146ceaaf439faed78ff763a851845
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-13 20:49:22 +0700

    update product

--- models/product.model.go
+++ models/product.model.go
@@ -1,6 +1,6 @@
 package models
 
-import "github.com/satori/go.uuid"
+import uuid "github.com/satori/go.uuid"
 
 type Product struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
@@ -10,9 +10,24 @@ type Product struct {
 	Type      string    `gorm:"type:varchar(15);not null"`
 }
 
-type UpdateProduct struct {
+type CreateProduct struct {
+	ID        uuid.UUID
 	Name      string `gorm:"type:varchar(55);not null"`
 	Amount    int    `gorm:"not null"`
 	UnitPrice int    `gorm:"not null"`
 	Type      string `gorm:"type:varchar(15);not null"`
+}
+
+type UpdateProduct struct {
+	Name      string `gorm:"type:varchar(55);not null"`
+	Amount    int    `gorm:"not null"`
+	UnitPrice int    `gorm:"not null"`
+}
+
+type DeleteProduct struct {
+	ID        uuid.UUID
+	Name      string `jorm:"type:varchar(55);not null"`
+	Amount    int    `jorm:"not null"`
+	UnitPrice int    `jorm:"not null"`
+	Type      string `jorm:"type:varchar(15);not null"`
 }
