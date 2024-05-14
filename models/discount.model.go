c121b14af8e7298d797deb0a758ab6550331271f
TewaranChaiyakit <rungoodnam@gmail.com>
2024-05-14 18:11:07 +0700

    discount

--- models/discount.model.go
+++ models/discount.model.go
@@ -1,1 +1,13 @@
 package models
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type Discount struct {
+	ID      uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Percent int       `gorm:"not null"`
+	Expir   time.Time
+}
