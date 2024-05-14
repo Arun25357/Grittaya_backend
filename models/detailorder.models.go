8de86ce1d2c0c2c74121fbef108e09d34fbc7a61
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-14 18:19:03 +0700

    Update detailorder.models.go

--- models/detailorder.models.go
+++ models/detailorder.models.go
@@ -3,7 +3,12 @@ package models
 import "github.com/satori/go.uuid"
 
 type Detailorder struct {
-	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Username string    `gorm:"type:varchar(55);uniqueIndex;not null"`
-	Password string    `gorm:"uniqueIndex;not null"`
+	ID       		uuid.UUID 	`gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	DeliveryType	string 		`gorm:"type:varchar(55);uniqueIndex;not null"`
+	TotalPrice		int 		`gorm:"not null"`
+	PaymentType		string		`gorm:"type:varchar(55);uniqueIndex;not null"`
+	Discount		string		`gorm:"type:varchar(55);uniqueIndex;not null"`
+	OrderID			uuid.UUID 	`gorm:"type:varchar(255);not null"`
+	SetproductID	uuid.UUID 	`gorm:"type:varchar(255);not null"`
+	CustomerID		uuid.UUID 	`gorm:"type:varchar(255);not null"`
 }
