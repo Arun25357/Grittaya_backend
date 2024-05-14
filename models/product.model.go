497de24107fda6b9f9c20f11d49c2a7c30134bb0
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-14 19:07:12 +0700

    Update product.model.go

--- models/product.model.go
+++ models/product.model.go
@@ -3,31 +3,39 @@ package models
 import uuid "github.com/satori/go.uuid"
 
 type Product struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Name      string    `gorm:"type:varchar(55);not null"`
-	Amount    int       `gorm:"not null"`
-	UnitPrice int       `gorm:"not null"`
-	Type      string    `gorm:"type:varchar(15);not null"`
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Name        string    `gorm:"type:varchar(55);not null"`
+	Amount      int       `gorm:"not null"`
+	UnitPrice   int       `gorm:"not null"`
+	Type        string    `gorm:"type:varchar(15);not null"`
+	Category    string    `gorm:"type:varchar(15);not null"`
+	Description string    `gorm:"type:varchar(15);not null"`
 }
 
 type CreateProduct struct {
-	ID        uuid.UUID
-	Name      string `jorm:"type:varchar(55);not null"`
-	Amount    int    `jorm:"not null"`
-	UnitPrice int    `jorm:"not null"`
-	Type      string `jorm:"type:varchar(15);not null"`
+	ID          uuid.UUID
+	Name        string `jorm:"type:varchar(55);not null"`
+	Amount      int    `jorm:"not null"`
+	UnitPrice   int    `jorm:"not null"`
+	Type        string `jorm:"type:varchar(15);not null"`
+	Category    string `jorm:"type:varchar(15);not null"`
+	Description string `jorm:"type:varchar(15);not null"`
 }
 
 type UpdateProduct struct {
-	Name      string `jorm:"type:varchar(55);not null"`
-	Amount    int    `jorm:"not null"`
-	UnitPrice int    `jorm:"not null"`
+	Name        string `jorm:"type:varchar(55);not null"`
+	Amount      int    `jorm:"not null"`
+	UnitPrice   int    `jorm:"not null"`
+	Category    string `jorm:"type:varchar(15);not null"`
+	Description string `jorm:"type:varchar(15);not null"`
 }
 
 type DeleteProduct struct {
-	ID        uuid.UUID
-	Name      string `jorm:"type:varchar(55);not null"`
-	Amount    int    `jorm:"not null"`
-	UnitPrice int    `jorm:"not null"`
-	Type      string `jorm:"type:varchar(15);not null"`
+	ID          uuid.UUID
+	Name        string `jorm:"type:varchar(55);not null"`
+	Amount      int    `jorm:"not null"`
+	UnitPrice   int    `jorm:"not null"`
+	Type        string `jorm:"type:varchar(15);not null"`
+	Category    string `jorm:"type:varchar(15);not null"`
+	Description string `jorm:"type:varchar(15);not null"`
 }
