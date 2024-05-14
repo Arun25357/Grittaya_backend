de6f55e34995606dde515f542ed701110c42ebd0
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-14 19:07:02 +0700

    Update detailorder.models.go

--- models/detailorder.models.go
+++ models/detailorder.models.go
@@ -1,14 +1,14 @@
 package models
 
-import "github.com/satori/go.uuid"
+import uuid "github.com/satori/go.uuid"
 
 type Detailorder struct {
-	ID       		uuid.UUID 	`gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	DeliveryType	string 		`gorm:"type:varchar(55);uniqueIndex;not null"`
-	TotalPrice		int 		`gorm:"not null"`
-	PaymentType		string		`gorm:"type:varchar(55);uniqueIndex;not null"`
-	Discount		string		`gorm:"type:varchar(55);uniqueIndex;not null"`
-	OrderID			uuid.UUID 	`gorm:"type:varchar(255);not null"`
-	SetproductID	uuid.UUID 	`gorm:"type:varchar(255);not null"`
-	CustomerID		uuid.UUID 	`gorm:"type:varchar(255);not null"`
+	ID           uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	DeliveryType string    `gorm:"type:varchar(55);uniqueIndex;not null"`
+	TotalPrice   int       `gorm:"not null"`
+	PaymentType  string    `gorm:"type:varchar(55);uniqueIndex;not null"`
+	Discount     string    `gorm:"type:varchar(55);uniqueIndex;not null"`
+	OrderID      uuid.UUID `gorm:"type:varchar(255);not null"`
+	SetproductID uuid.UUID `gorm:"type:varchar(255);not null"`
+	CustomerID   uuid.UUID `gorm:"type:varchar(255);not null"`
 }
