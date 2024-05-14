3cd1aa11bfe3603b20d78d4bd882ec1f93a48b82
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-14 18:34:01 +0700

    Update stockforsale.model.go

--- models/stockforsale.model.go
+++ models/stockforsale.model.go
@@ -1,1 +1,13 @@
 package models
+
+import uuid "github.com/satori/go.uuid"
+
+type Stockforsale struct {
+	ID           uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Name         string    `gorm:"type:varchar(55);not null"`
+	Amount       int       `gorm:"not null"`
+	UnitPrice    int       `gorm:"not null"`
+	Status       int
+	Category     string    `gorm:"type:varchar(55);not null"`
+	SetproductID uuid.UUID `gorm:"type:varchar(255);not null"`
+}
