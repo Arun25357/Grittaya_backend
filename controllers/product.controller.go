131dd490fa945074cacc0365b2b9d3081fc3a40b
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-21 17:33:28 +0700

    Update product.controller.go

--- controllers/product.controller.go
+++ controllers/product.controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	"gorm.io/gorm"
+)
+
+type ProductController struct {
+	DB *gorm.DB
+}
+
+func NewProductController(DB *gorm.DB) ProductController {
+	return ProductController{DB: DB}
+}
