6ff1f5e7528aac25ee20763d4fcfa909ccf6f1a6
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-23 16:40:35 +0700

    Update customer.model.go

--- models/customer.model.go
+++ models/customer.model.go
@@ -6,7 +6,6 @@ type Customer struct {
 	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	Username string    `gorm:"type:varchar(55);not null"`
 	Phone    string    `gorm:"type:varchar(15);not null"`
-	Email    string    `gorm:"type:varchar(55);uniqueIndex;not null"`
 	Location string    `gorm:"not null"`
 	Postcode int       `gorm:"not null"`
 }
