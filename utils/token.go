9e8f65c81bdc2c2944a921054d584b1669e7698e
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-29 10:37:05 +0700

    Update token.go

--- utils/token.go
+++ utils/token.go
@@ -49,3 +49,27 @@ func ValidateToken(token string, signedJWTKey string) (interface{}, error) {
 
 	return claims["sub"], nil
 }
+
+func GetUserIDFromToken(token string, signedJWTKey string) (string, error) {
+	tok, err := jwt.Parse(token, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", jwtToken.Header["alg"])
+		}
+		return []byte(signedJWTKey), nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("invalidate token: %w", err)
+	}
+
+	claims, ok := tok.Claims.(jwt.MapClaims)
+	if !ok || !tok.Valid {
+		return "", fmt.Errorf("invalid token claim")
+	}
+
+	userID, ok := claims["sub"].(string)
+	if !ok {
+		return "", fmt.Errorf("invalid user ID in token")
+	}
+
+	return userID, nil
+}
